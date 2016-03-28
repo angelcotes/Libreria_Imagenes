28c9a1a4874ca9815cf3ef6c68699549bc0293b0
unknown <angelcotes@gmail.com>
2016-03-28 01:06:08 -0500

    mejora en todas las opciones. Reduccion de codigo

--- ImageLibrary/app/src/main/java/angelcotes/imagelibrary/MainActivity.java
+++ ImageLibrary/app/src/main/java/angelcotes/imagelibrary/MainActivity.java
@@ -6,21 +6,25 @@ import android.graphics.Bitmap;
 import android.graphics.Matrix;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageView;
-
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.security.auth.callback.Callback;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String fileName = null;
     private ImageView imageView;
     private int index = 0;
     private int degree = 0;
@@ -36,51 +40,22 @@ public class MainActivity extends AppCompatActivity {
 
 
         String imageUrl = "http://descargararesparaandroid.com/wp-content/uploads/2013/10/musica-para-android.jpg";
-
-        picasso = new Picasso.Builder(this).listener(new Picasso.Listener() {
-            @Override
-            public void onImageLoadFailed(Picasso picasso, Uri uri, Exception exception) {
-                exception.printStackTrace();
-            }
-        }).build();
-        picasso.load(imageUrl).into(imageView, new com.squareup.picasso.Callback() {
-            @Override
-            public void onSuccess() {
-
-            }
-
-            @Override
-            public void onError() {
-
-            }
-        });
-
     }
 
     public void Image_touch(View view) {
 
-        final CharSequence[] options = {"Change Image", "Cut Image", "Rotate 90°", "Save Image", "Cancel"};
+        final CharSequence[] options = {"Change Image", "Cut Image", "Rotate 90°", "Save Image"};
         final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
 
         builder.setTitle("Opciones");
         builder.setItems(options, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int pick) {
-                switch (pick){
+                switch (pick) {
                     case 0:
                         degree = 0;
                         imageTemp = imagesWEb[index].toString();
-                        picasso.load(imagesWEb[index].toString()).into(imageView, new com.squareup.picasso.Callback() {
-                            @Override
-                            public void onSuccess() {
-
-                            }
-
-                            @Override
-                            public void onError() {
-
-                            }
-                        });
+                        Picasso.with(MainActivity.this).load(imageTemp).into(imageView);
                         if (index + 1 >= imagesWEb.length)
                             index = 0;
                         else
@@ -88,59 +63,19 @@ public class MainActivity extends AppCompatActivity {
                         break;
                     case 1:
                         degree = 0;
-                        picasso.load(imageTemp).resize(imageView.getWidth(),imageView.getHeight()).into(imageView, new com.squareup.picasso.Callback(){
-
-                            @Override
-                            public void onSuccess() {
-
-                            }
-
-                            @Override
-                            public void onError() {
-
-                            }
-                        });
+                        Picasso.with(MainActivity.this).load(imageTemp).resize(imageView.getWidth()/2, imageView.getHeight()/2).into(imageView);
                         break;
                     case 2:
-                        picasso.load(imageTemp).rotate(degree + 90).into(imageView, new com.squareup.picasso.Callback(){
-
-                            @Override
-                            public void onSuccess() {
-
-                            }
-
-                            @Override
-                            public void onError() {
-
-                            }
-                        });
+                        Picasso.with(MainActivity.this).load(imageTemp).rotate(degree + 90).into(imageView);
                         if (degree + 90 > 359)
                             degree = 0;
-                        else
-                        {
+                        else {
                             degree = degree + 90;
                         }
                         break;
                     case 3:
-                        picasso.load(imageTemp).into(new Target() {
-                            @Override
-                            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
-
-                            }
-
-                            @Override
-                            public void onBitmapFailed(Drawable errorDrawable) {
-
-                            }
-
-                            @Override
-                            public void onPrepareLoad(Drawable placeHolderDrawable) {
-
-                            }
-                        });
-                        break;
-                    case 4:
-
+                        File file = new File(Environment.getExternalStorageDirectory(),"MyPhoto/data/com.usd.pop");
+                        Picasso.with(MainActivity.this).load(imageTemp).into((Target) file);
                         break;
                     default:
                         dialog.dismiss();
@@ -149,5 +84,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         builder.show();
+
     }
+
 }
